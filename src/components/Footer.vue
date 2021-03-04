283fda1e4b0a8ea257372f08c4359d014c3fd56a
Alicia <alicia@loka.com>
2021-03-04 15:02:51 +0000

    footer fix

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -11,7 +11,12 @@ export default defineComponent({
 
 <style scoped>
 .footer {
+  position:fixed;
+  bottom:0;
   padding: 30px;
   background-color:wheat;
+  margin-top: 10px;
+  right: 0;
+  left: 0;
 }
 </style>
