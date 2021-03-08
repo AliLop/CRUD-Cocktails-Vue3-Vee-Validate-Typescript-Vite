3b95862c270020bba64d76fa9726e8f17a942af4
Alicia <alicia@loka.com>
2021-03-08 11:41:46 +0000

    fix Drink import

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,49 +1,48 @@
 <template>
-        <div class="card">
-            <img alt="Cocktail image" :src="imgUrl(drink)" /> 
+  <div class="card">
+    <img alt="Cocktail image" :src="imgUrl(drink)" />
 
-            <h4 class="title">{{ drink.strDrink }}</h4>
-          
-            <router-link 
-                :to="{ name: 'DetailsView', params: { id: drink.idDrink } }"
-            >
-            <button>See details</button>
-            </router-link>
+    <h4 class="title">{{ drink.strDrink }}</h4>
 
-            <button class="fav-button"
-                v-if="!isDrinkFavorite(drink.idDrink)"
-                @click.prevent="addFav(drink)"
-                type="button"
-            >Add to favorites</button>
-            <button class="fav-button"
-                v-if="isDrinkFavorite(drink.idDrink)"
-                @click.prevent="removeFav(drink.idDrink)"
-                type="button"
-            >Remove favorite</button>
-        </div>
+    <router-link :to="{ name: 'DetailsView', params: { id: drink.idDrink } }">
+      <button>See details</button>
+    </router-link>
+
+    <button
+      class="fav-button"
+      v-if="!isDrinkFavorite(drink.idDrink)"
+      @click.prevent="addFav(drink)"
+      type="button"
+    >
+      Add to favorites
+    </button>
+    <button
+      class="fav-button"
+      v-if="isDrinkFavorite(drink.idDrink)"
+      @click.prevent="removeFav(drink.idDrink)"
+      type="button"
+    >
+      Remove favorite
+    </button>
+  </div>
 </template>
 
 <script lang="ts">
-import { Drink } from '../../models/drink';
-import { PropType } from 'vue';
-import { mapActions, mapGetters } from 'vuex';
+  import { Drink } from '../models/drink';
+  import { PropType } from 'vue';
+  import { mapActions, mapGetters } from 'vuex';
 
-export default {
+  export default {
     name: 'Card',
     props: { drink: { type: Object as PropType<Drink> } },
     methods: {
-        imgUrl(drink: Drink) {
-        return `${drink.strDrinkThumb}/preview`
-        },
-        ...mapActions([
-            'addFav',
-            'removeFav'
-        ]),
+      imgUrl(drink: Drink) {
+        return `${drink.strDrinkThumb}/preview`;
+      },
+      ...mapActions(['addFav', 'removeFav']),
     },
     computed: {
-        ...mapGetters([
-            'isDrinkFavorite'
-        ])
+      ...mapGetters(['isDrinkFavorite']),
     },
-}
+  };
 </script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,80 +1,80 @@
 <template>
-    <header> 
-      <nav>
-        <router-link to="/">Cocktails</router-link>
-        <router-link to="/favorites">Favorites ({{ favoritesCount() }})</router-link>
-        <router-link to="/contact-us">Contact us</router-link>    
-      </nav>
-      <div class="routing">
-        <button @click="back"><i class="fas fa-arrow-left"></i></button>
-        <button @click="redirect"><i class="fas fa-cocktail"></i></button>
-        <button @click="forward"><i class="fas fa-arrow-right"></i></button>
-      </div>
-    </header>
+  <header>
+    <nav>
+      <router-link to="/">Cocktails</router-link>
+      <router-link to="/favorites"
+        >Favorites ({{ favoritesCount() }})</router-link
+      >
+      <router-link to="/contact-us">Contact us</router-link>
+    </nav>
+    <div class="routing">
+      <button @click="back"><i class="fas fa-arrow-left"></i></button>
+      <button @click="redirect"><i class="fas fa-cocktail"></i></button>
+      <button @click="forward"><i class="fas fa-arrow-right"></i></button>
+    </div>
+  </header>
 </template>
 
 <script lang="ts">
-import { mapGetters} from 'vuex'; 
+  import { mapGetters } from 'vuex';
 
-export default {
-  name: 'Header',
-  methods: {
-    redirect() {
-      this.$router.push({ name: 'HomeView' })
+  export default {
+    name: 'Header',
+    methods: {
+      redirect() {
+        this.$router.push({ name: 'HomeView' });
+      },
+      back() {
+        this.$router.go(-1);
+      },
+      forward() {
+        this.$router.go(+1);
+      },
+      ...mapGetters(['favoritesCount']),
     },
-    back() {
-      this.$router.go(-1)
-    },
-    forward() {
-      this.$router.go(+1)
-    },
-    ...mapGetters([
-      'favoritesCount'
-    ])
-  },
-}
+  };
 </script>
 
 <style scoped>
-header {
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
-  padding: 2vh 0; 
-  background-color: var(--bgLight);
-}
-nav {
-  display: flex;
-}
-nav a {
-  font-weight: bold;
-  color: var(--titleColor); 
-  text-decoration: none;
-  padding: 10px 20px;
-  border-radius: 4px;
-}
-nav a.router-link-active {
+  header {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    padding: 2vh 0;
+    background-color: var(--bgLight);
+  }
+  nav {
+    display: flex;
+  }
+  nav a {
+    font-weight: bold;
+    color: var(--titleColor);
+    text-decoration: none;
+    padding: 10px 20px;
+    border-radius: 4px;
+  }
+  nav a.router-link-active {
     color: var(--lightColor);
     font-size: large;
-}
-.routing {
-  display: flex;
-}
-@media screen and (max-width: 360px) {
-  .routing {
-    display: none;
   }
-}
-button {
-  margin: 0px 10px;
-  background-color: var(--bgLight);
-  background-image: none;
-  font-size: larger;
-}
-button:focus {
+  .routing {
+    display: flex;
+  }
+  @media screen and (max-width: 360px) {
+    .routing {
+      display: none;
+    }
+  }
+  button {
+    margin: 0px 10px;
+    background-color: var(--bgLight);
+    background-image: none;
+    font-size: larger;
+  }
+  button:focus {
     outline: none;
-}
-i {
-  color: var(--lightColor);
-}
+  }
+  i {
+    color: var(--lightColor);
+  }
 </style>
